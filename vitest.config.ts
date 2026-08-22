0aac9a187c14de5d17e01e1cc7aea4714f6769f9
Ivan <sarapuraivan25@gmail.com>
2026-08-22 11:28:19 +0200

    feat(dx): Some details

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
@@ -8,7 +8,6 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
-    exclude: [...configDefaults.exclude, "e2e/**"],
   },
   resolve: {
     alias: {
